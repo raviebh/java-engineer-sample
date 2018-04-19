9bb6787c71a3fd9af826697eebaabdd198c03df9
ppedasing <34035637+ppedasing@users.noreply.github.com>
2018-04-19 15:47:39 -0500

    Update UserResource.java

--- src/main/java/com/att/demo/resource/UserResource.java
+++ src/main/java/com/att/demo/resource/UserResource.java
@@ -5,7 +5,58 @@ package com.att.demo.resource;
  * 
  * 
  */
+@Api("user")
+@Path("/users")
 public interface UserResource {
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_JSON})
+    @ApiOperation(
+            value = "Get Account Resource",
+            notes = "Returns all the accounts in ResourceCollection representation format",
+            response = AccountResponse.class)
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 200, message = "OK"),
+                    @ApiResponse(code = 204, message = "No Content")
+            })
+    public Response findAllUsers();
+
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_JSON})
+    @ApiOperation(
+            value = "Get Account Resource",
+            notes = "Returns all the accounts in ResourceCollection representation format",
+            response = UserResponse.class)
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 201, message = "OK"),
+                    @ApiResponse(code = 404, message = "No Content Found")
+
+            })
+
+    @Path(value = "/{id}")
+    public Response getUser(@PathParam("id")  Integer id);
+
+
+    @POST
+    @Produces({MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_JSON})
+    @ApiOperation(
+            value = "Get Account Resource",
+            notes = "Returns all the accounts in ResourceCollection representation format",
+            response = UserResponse.class)
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 200, message = "OK"),
+                    @ApiResponse(code = 409, message = "CONFLICTED")
+
+            })
+
+    public Response createUser(@RequestBody User user);
 	
    
 	
